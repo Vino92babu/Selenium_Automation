afe7829ccd86cc53d6694da575f5202f2d0e8519
vino9 <vino9@Vinoth.bbrouter>
2023-01-06 01:36:35 +0530

    Intial

--- Selenium/OrangeHRM_login.py
+++ Selenium/OrangeHRM_login.py
@@ -3,3 +3,4 @@ driver = webdriver.Chrome("C:\Python3.9.10\Scripts\chromedriver.exe")
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 driver.maximize_window()
 driver.find_element_by_name("username").send_keys("Vino")
+def
